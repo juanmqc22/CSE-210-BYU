d7f8bc3fa14908854555c4f387cea7bddc4d049d
Juan Quezada <juanmqc22@gmail.com>
2022-11-10 20:48:23 -0300

    Update

---
+++ csharp-prep/Prep1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prep1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is prep 1");
+
+            Console.Write("What is your first name? ");
+            string first = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string last = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last}, {first} {last}");
+        }
+    }
+}
+        
+    
+
